bddb0bab52a7fcd85673a4ba8106469ea931c706
laiacamara <146718844+laiacamara@users.noreply.github.com>
2024-12-28 18:38:26 +0100

    Update PlayList.py

--- PlayList.py
+++ PlayList.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#PlayList.py : ** REQUIRED ** El vostre codi de la classe PlayList.
+# PlayList.py : ** REQUIRED ** El vostre codi de la classe PlayList.
 
 import cfg # Necessari per a la pràctica !!
  # Mireu el contingut de l'arxiu
@@ -13,55 +13,53 @@ import os
 
 class PlayList:
    
-    def __init__ (self, video_id, video_player):
-       
-        self._videoid = video_id
-        self._videoplayer = video_player
-        self._playlist = []
+    __slots__=['__videoid','__videoplayer','__playlist']
+
+    def __init__(self, video_id, video_player):
+        self.__videoid = video_id
+        self.__videoplayer = video_player
+        self.__playlist = []
     
-    __slots__='_videoid','_videoplayer','_playlist'
-   
-    def load_file (self, file:str):
-        self._playlist = []
+
+    def load_file(self, file: str):
+        self.__playlist = []
         if not file.endswith(".m3u"):
             return
                 
-        with open(file, "r", errors = 'ignore') as fitxer:
+        with open(file, "r", errors='ignore') as fitxer:
             for linia in fitxer:
                 linia = linia.strip()
                 if linia and not linia.startswith("#") and linia.endswith(".mp4"):
-                    uuid = self._videoid.get_uuid(linia)
+                    uuid = self.__videoid.get_uuid(linia)
                     if uuid:
-                        self._playlist.append(uuid)
+                        self.__playlist.append(uuid)
        
-        return self._playlist
+        return self.__playlist
            
-       
-       
-
-    def play(self, mode= int):
-        if self._playlist:
-            for uuid in self._playlist:
-                self._videoplayer.play_video(uuid,mode)
+    def play(self, mode=int):
+        if self.__playlist:
+            for uuid in self.__playlist:
+                self.__videoplayer.play_video(uuid, mode)
    
-    def add_video_at_end(self,uuid: str):
-        self._playlist.append(uuid)
-       
-
-    def remove_first_video (self):
-        if self._playlist:
-            del self._playlist[0]
+    def add_video_at_end(self, uuid: str):
+        self.__playlist.append(uuid)
+   
+    def remove_first_video(self):
+        if self.__playlist:
+            del self.__playlist[0]
    
     def remove_last_video(self):
-        if self._playlist:
-            self._playlist.pop()
-   
+        if self.__playlist:
+            self.__playlist.pop()
    
     def __len__(self):
-        return len(self._playlist)
+        return len(self.__playlist)
     
     def __iter__(self):
-        return iter(self._playlist)
+        return iter(self.__playlist)
     
     def __str__(self):
-        return str(self._playlist)
+        return str(self.__playlist)
+    
+    def __repr__(self):
+        return self.__playlist
