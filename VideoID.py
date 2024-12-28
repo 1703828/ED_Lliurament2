3b4f62ab24d065a41122d728046f3388e685512f
laiacamara <146718844+laiacamara@users.noreply.github.com>
2024-12-28 18:39:24 +0100

    Update VideoID.py

--- VideoID.py
+++ VideoID.py
@@ -1,31 +1,29 @@
 import uuid
 import os
 
-import uuid
-
-
 class VideoID:
     """
     Gestiona el parell file-path <-> UUID per a arxius MP4.
     """
+    __slots__ = ['__file_uuid_map']
 
     def __init__(self):
-        self._file_uuid_map = {}
+        self.__file_uuid_map = {}
 
     def generate_uuid(self, file: str) -> str:
         """ Genera un nou UUID per a un arxiu si encara no existeix. """
         new_uuid = uuid.uuid5(uuid.NAMESPACE_URL, file)
         # Verificar si el UUID ja existeix directament
-        if new_uuid in self._file_uuid_map:
+        if new_uuid in self.__file_uuid_map:
             print('COL·LISIÓ: Identificador utilitzat anteriorment, aquest UUID no serà utilitzat.')
         else:
-            self._file_uuid_map[new_uuid] = file
+            self.__file_uuid_map[new_uuid] = file
             return str(new_uuid)
 
     def get_uuid(self, file: str) -> str:
         """ Retorna el UUID associat a un fitxer, si existeix. """
         # Verificar si el fitxer existeix directament
-        for uuid, f in self._file_uuid_map.items():
+        for uuid, f in self.__file_uuid_map.items():
             if f == file:
                 return str(uuid)
 
@@ -37,13 +35,21 @@ class VideoID:
             print(f"Error: '{id_uuid}' no és un UUID vàlid.")
             return
         # Verificar si el UUID existeix directament
-        if file_uuid in self._file_uuid_map:
-            del self._file_uuid_map[file_uuid]
+        if file_uuid in self.__file_uuid_map:
+            del self.__file_uuid_map[file_uuid]
 
     def __len__(self):
         """ Retorna el nombre d'UUIDs registrats. """
-        return len(self._file_uuid_map)
+        return len(self.__file_uuid_map)
 
     def __str__(self):
         """ Representació en cadena de VideoID. """
         return f"VideoID managing {len(self)} UUIDs"
+    
+    def __iter__(self):
+        """ Permet iterar sobre els UUIDs registrats. """
+        return iter(self.__file_uuid_map.keys())
+        
+    def __repr__(self):
+        """ Representació en cadena de VideoID. """
+        return str(self.__file_uuid_map)
